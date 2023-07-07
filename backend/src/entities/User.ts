c1c47a26745479f5f0fbe2f8c811cdef546101e5
joaopaulu <jptick@gmail.com>
2023-07-07 11:38:35 -0300

    Criando Migrantion e Entidade User

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,41 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  first: string;
+
+  @Column()
+  last: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  picture: string;
+
+  @Column()
+  street: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  postcode: number;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
